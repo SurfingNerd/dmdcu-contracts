c259829d95b1960c440bc6576a954732f54064be
Thomas Haller <Thomas.Haller@lab10.coop>
2019-08-06 16:45:28 +0200

    added ModdedMoto

--- contracts/DMDCertifiedUnique.sol
+++ contracts/DMDCertifiedUnique.sol
@@ -15,4 +15,39 @@ contract DMDCertifiedUnique is ERC721 {
         //main ethereum address used by the certifier.
         address mainAddress;
     }
+
+    //represents a modified motorcycle
+    struct ModdedMoto {
+
+        address owner;
+        bytes32 name;
+        bytes32 nameBaseModel;
+
+        string modificationPlainText;
+
+        //IPFS Hash with the imageRessources. (to be defined what is expected)
+        bytes32 imageRessourcesIPFSAddress;
+
+        uint32 successorID;
+        uint32 predecessorID;
+        uint32 certifierID;
+
+        //all values are meant to have 3 dots precision. example: 86.731 horse power = 86731
+        uint32 horsepower;
+        uint32 weight;
+        uint32 topSpeed;
+
+        //modification time: unix time with 64 bit to bypass Y2038 problem (https://en.wikipedia.org/wiki/Unix_time https://en.wikipedia.org/wiki/Year_2038_problem)
+        uint64 modificationDate;
+
+        //0: no vintage
+        //1: has vintage elements
+        //2: pure vintage
+        uint8 vintageGrade;
+
+        //0: old base
+        //1: modern bike
+        //2: state of the art high end technology
+        uint8 techGrade;
+    }
 }
